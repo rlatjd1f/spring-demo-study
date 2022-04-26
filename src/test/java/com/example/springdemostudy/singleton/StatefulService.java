2a07d30555325932c35d23c4910f7dff0ff081dc
ksr93 <dl290382@gmail.com>
2022-04-26 17:59:26 +0900

    Singleton Container stateful Test

---
+++ src/test/java/com/example/springdemostudy/singleton/StatefulService.java
@@ -0,0 +1,9 @@
+package com.example.springdemostudy.singleton;
+
+public class StatefulService {
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }
+}
