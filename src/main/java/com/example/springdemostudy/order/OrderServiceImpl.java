08dcaadccdc4c918fb6ee8b644c5fa6772f2d2dd
ksr93 <dl290382@gmail.com>
2022-05-17 09:48:16 +0900

    @Primary 우선순위 의존관계 주입

--- src/main/java/com/example/springdemostudy/order/OrderServiceImpl.java
+++ src/main/java/com/example/springdemostudy/order/OrderServiceImpl.java
@@ -3,19 +3,15 @@ package com.example.springdemostudy.order;
 import com.example.springdemostudy.discount.DiscountPolicy;
 import com.example.springdemostudy.member.Member;
 import com.example.springdemostudy.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
