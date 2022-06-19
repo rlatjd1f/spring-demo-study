75d4229de45a3908f9aec6b95607bacc22bc46fa
rlatj <dl290382@gmail.com>
2022-06-19 19:22:55 +0900

    web scope, proxy mode

--- src/main/java/com/example/springdemostudy/web/LogDemoController.java
+++ src/main/java/com/example/springdemostudy/web/LogDemoController.java
@@ -2,7 +2,6 @@ package com.example.springdemostudy.web;
 
 import com.example.springdemostudy.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
